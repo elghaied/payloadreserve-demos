33b6287f33be6cf811a7028d5bb56afad852159d
elghaied <elghaied.eslam@gmail.com>
2026-03-14 01:45:53 +0100

    feat(events): add sidebar layout to account section

--- apps/events/src/app/(frontend)/[locale]/(customer)/account/layout.tsx
+++ apps/events/src/app/(frontend)/[locale]/(customer)/account/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation'
 import { getPayload } from 'payload'
 import { headers as getHeaders } from 'next/headers'
 import config from '@payload-config'
+import { AccountSidebar } from '@/components/account/AccountSidebar'
 
 type Props = {
   children: React.ReactNode
@@ -21,5 +22,14 @@ export default async function AccountLayout({ children, params }: Props) {
     redirect(`/${locale}/login`)
   }
 
-  return <>{children}</>
+  return (
+    <section className="px-6 py-16 lg:px-12 lg:py-24">
+      <div className="flex flex-col gap-8 lg:flex-row lg:gap-12">
+        <div className="flex-shrink-0 lg:w-48">
+          <AccountSidebar locale={locale} />
+        </div>
+        <div className="min-w-0 flex-1">{children}</div>
+      </div>
+    </section>
+  )
 }
